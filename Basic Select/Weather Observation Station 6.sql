f89d60a57d8cc47b0e16d131dda6ed7850056e8b
30-RamisaHassan-78 <ramisa0811@gmail.com>
2025-01-22 00:58:50 -0500

    new sql

---
+++ Basic Select/Weather Observation Station 6.sql
@@ -0,0 +1,19 @@
+-- Problem Statement
+
+-- Query the list of CITY names starting with vowels (i.e., a, e, i, o, or u) from STATION. Your result cannot contain duplicates.
+
+-- -- The STATION table is described as follows:
+
+-- -- Field         Type
+----------------------
+-- ID            NUMBER
+-- City          VARCHAR2(17)
+-- State         VARCHAR2(3)
+-- Lat_N         Number
+-- Long_W        NUMBER
+
+-- where LAT_N is the northern latitude and LONG_W is the western longitude.
+
+SELECT DISTINCT CITY 
+FROM STATION
+WHERE CITY LIKE '[AEIOU]%'
